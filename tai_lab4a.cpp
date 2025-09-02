8ec57a55b1455f828a10eb5403cee4149b030b9a
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-09-02 14:03:48 -0700

    Preamble

--- tai_lab4a.cpp
+++ tai_lab4a.cpp
@@ -1,3 +1,21 @@
 // COMSC 210 | Lab 4a | Andrew Tai | Sep 2 2025
 // IDE: Visual Studio Code
 
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+// Struct for a single color
+struct Color {
+    int r;
+    int g;
+    int b;
+};
+
+// Fn prototypes
+Color createColor();
+
+// -------
+// Main
+// -------
