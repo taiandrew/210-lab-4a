a681757da4e7bfdd3ea5606b5dff93bb0fb8d377
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-09-02 14:07:30 -0700

    createColor fn; preamble fixes

--- tai_lab4a.cpp
+++ tai_lab4a.cpp
@@ -1,13 +1,16 @@
 // COMSC 210 | Lab 4a | Andrew Tai | Sep 2 2025
 // IDE: Visual Studio Code
 
-#include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <iomanip>
 #include <vector>
 
+using namespace std;
+
 // Struct for a single color
-struct Color {
+struct Color
+{
     int r;
     int g;
     int b;
@@ -19,3 +22,27 @@ Color createColor();
 // -------
 // Main
 // -------
+int main()
+{
+    // Randomly choose 25-50 colors to generate
+    int nColors = rand() % 26 + 25;
+
+}
+
+// -------
+// Programmer fns
+// -------
+Color createColor()
+{
+    /*
+    Create a color via random RGB vals
+    args: none
+    returns: Color
+    */
+
+    Color c;
+    c.r = rand() % 256;
+    c.g = rand() % 256;
+    c.b = rand() % 256;
+    return c;
+}
